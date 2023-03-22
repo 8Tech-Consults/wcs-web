e25594b22fbb73840925853abf37428067a6f9a6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-03-22 14:41:31 +0300

    romina

--- app/Admin/Controllers/CaseSuspectController.php
+++ app/Admin/Controllers/CaseSuspectController.php
@@ -243,13 +243,13 @@ class CaseSuspectController extends AdminController
 
         $grid->column('case_pa_id', __('P.A of case'))
             ->display(function ($x) {
-                return $this->case->pa->name;
 
-                if ($this->case->is_offence_committed_in_pa != 1) {
-                    return "-";
+
+                if ($this->case->is_offence_committed_in_pa == 'No') {
+                    return "NAPA";
                 }
                 if ($this->case->pa == null) {
-                    return  '-';
+                    return  'NAPA';
                 }
                 return $this->case->pa->name;
             })->hide();
@@ -371,7 +371,7 @@ class CaseSuspectController extends AdminController
 
         $grid->column('pa_id', 'P.A of Arrest ')
             ->display(function ($x) {
-                 
+
                 return $this->arrestPa->name;
             })
             ->sortable()

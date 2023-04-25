6e2c2de90f218f66af6cdf04f6a31a6a28c56454
Muhindo Mubaraka <mubs0x@gmail.com>
2023-04-26 02:22:25 +0300

    'exhibits'

--- app/Http/Controllers/ApiPostsController.php
+++ app/Http/Controllers/ApiPostsController.php
@@ -120,7 +120,7 @@ class ApiPostsController extends Controller
     public function index(Request $r)
     {
         $data =  CaseModel::where([])
-        ->with('suspects')
+        ->with('suspects','exhibits')
         ->orderBy('id', 'Desc')
         ->limit(25)->get(); 
         return $this->success($data, 'Success.');

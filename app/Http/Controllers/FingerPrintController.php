088605605bd5de46c9fb32d444bbab89f587c534
Muhindo Mubaraka <mubs0x@gmail.com>
2023-05-13 03:29:05 +0300

    min_suspects

--- app/Http/Controllers/FingerPrintController.php
+++ app/Http/Controllers/FingerPrintController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\CaseSuspect;
 use App\Models\Utils;
 use App\Traits\ApiResponser;
 use Carbon\Carbon;
@@ -24,19 +23,72 @@ class FingerPrintController extends Controller
      */
 
 
-
+     public function min_suspects(Request $r)
+     {
+         $suss = [];
+         foreach (CaseSuspect::all() as $key => $s) {
+             $sus['id'] = $s->id;
+             $sus['name'] = $s->name;
+             $sus['uwa_suspect_number'] = $s->uwa_suspect_number;
+             $suss[] = $sus;
+         }
+ 
+         die(json_encode($suss));
+     } 
 
 
     public function min_login(Request $r)
     {
-        $suss = [];
-        foreach (CaseSuspect::all() as $key => $s) {
-            $sus['id'] = $s->id;
-            $sus['name'] = $s->name;
-            $sus['uwa_suspect_number'] = $s->uwa_suspect_number;
-            $suss[] = $sus;
+        if ($r->phone_number == null) {
+            return $this->error('Email address is required.');
+        }
+        $phone_number = Utils::prepare_phone_number($r->phone_number);
+
+        if (!Utils::phone_number_is_valid($phone_number)) {
+            $phone_number = $r->phone_number;
+        }
+        if ($r->password == null) {
+            return $this->error('Password is required.');
         }
 
-        die(json_encode($suss));
+        $u = Administrator::where('phone_number_1', $phone_number)
+            ->orWhere('username', $phone_number)
+            ->orWhere('email', $phone_number)
+            ->first();
+        if ($u == null) {
+            return $this->error('User account not found.');
+        }
+
+        //auth('api')->factory()->setTTL(Carbon::now()->addMonth(12)->timestamp);
+
+        Config::set('jwt.ttl', 60 * 24 * 30 * 365);
+
+        $token = auth('api')->attempt([
+            'username' => $phone_number,
+            'password' => trim($r->password),
+        ]);
+
+
+        if ($token == null) {
+            $token = auth('api')->attempt([
+                'phone_number_1' => $phone_number,
+                'password' => trim($r->password),
+            ]);
+        }
+
+        if ($token == null) {
+            $token = auth('api')->attempt([
+                'email' => $phone_number,
+                'password' => trim($r->password),
+            ]);
+        }
+
+        if ($token == null) {
+            return $this->error('Wrong credentials.');
+        }
+        $u->token = $token;
+ 
+
+        return $this->success($u->id, 'Logged in successfully.');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::POST("users/register", [ApiAuthController::class, "register"]);
 Route::POST("users/login", [ApiAuthController::class, "login"]);
-Route::POST("min/login", [FingerPrintController::class, "min_login"]);
+Route::POST("users/min/login", [FingerPrintController::class, "min_login"]);
+Route::POST("users/min/suspects", [FingerPrintController::class, "min_suspects"]);
 Route::POST("temp-data", function (Request $r) {
     if (
         $r->user_id == null ||

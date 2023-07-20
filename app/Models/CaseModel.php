7999849c700cb5f0024a7c16b54c2161883d4ff8
Dali Hillary <dalirichardh@gmail.com>
2023-07-20 17:26:48 +0300

    fix default district

--- app/Models/CaseModel.php
+++ app/Models/CaseModel.php
@@ -65,6 +65,7 @@ class CaseModel extends Model
             ) {
                 $m->is_offence_committed_in_pa = 'Yes';
                 $m->ca_id = $pa->ca_id;
+                $m->district_id = 0; //Default district is 0
             } else {
                 $m->is_offence_committed_in_pa = 'No';
                 $m->pa_id = 1;
@@ -86,6 +87,7 @@ class CaseModel extends Model
             ) {
                 $m->is_offence_committed_in_pa = 'Yes';
                 $m->ca_id = $pa->ca_id;
+                $m->district_id = 0; //Default district is 0
             } else {
                 $m->is_offence_committed_in_pa = 'No';
                 $m->pa_id = 1;

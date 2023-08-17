e87cac55657147ae5477165eb7a2ee14eab91b5e
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-17 09:06:01 +0300

    new

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -5,7 +5,6 @@ namespace App\Http\Middleware;
 use Closure;
 use JWTAuth;
 use Exception;
-use Tymon\JWTAuth\Facades\JWTAuth as FacadesJWTAuth;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
 
 class JwtMiddleware extends BaseMiddleware
@@ -20,17 +19,7 @@ class JwtMiddleware extends BaseMiddleware
      */
     public function handle($request, Closure $next)
     {
-        try {
-            $user = FacadesJWTAuth::parseToken()->authenticate();
-        } catch (Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
-                return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
-                return response()->json(['status' => 'Token is Expired']);
-            }else{
-                return response()->json(['status' => 'Authorization Token not found']);
-            }
-        }
+        App\Http\Middleware\JWTAuth::parseToken()->authenticate();
         return $next($request);
     }
 }

e36d7b67dd27e36b068fe0aaabf1474e3594ac60
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-17 10:23:13 +0300

    return $next($request);

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -7,6 +7,7 @@ use JWTAuth;
 use Exception;
 use Tymon\JWTAuth\Facades\JWTAuth as FacadesJWTAuth;
 use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+use Illuminate\Support\Str;
 
 class JwtMiddleware extends BaseMiddleware
 {
@@ -26,30 +27,33 @@ class JwtMiddleware extends BaseMiddleware
 
     public function handle($request, Closure $next)
     {
-        if(!$request->expectsJson()){
+        if (!$request->expectsJson()) {
             return $next($request);
         }
-        
 
-        var_dump($_SERVER['REQUEST_URI']);
-        die();
 
+        if (
+            Str::contains($_SERVER['REQUEST_URI'], 'login') ||
+            Str::contains($_SERVER['REQUEST_URI'], 'register')
+        ) {
+            return $next($request);
+        }
 
 
         //$request->headers->set('Authorization', $headers['authorization']);// set header in request
         try {
             //$headers = apache_request_headers(); //get header
             $headers = getallheaders(); //get header
-            $request->headers->set('Authorization', $headers['Authorizations']);// set header in request
-            $request->headers->set('authorization', $headers['Authorizations']);// set header in request
+            $request->headers->set('Authorization', $headers['Authorizations']); // set header in request
+            $request->headers->set('authorization', $headers['Authorizations']); // set header in request
 
             $user = FacadesJWTAuth::parseToken()->authenticate();
         } catch (Exception $e) {
-            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException){
+            if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException) {
                 return response()->json(['status' => 'Token is Invalid']);
-            }else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException){
+            } else if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException) {
                 return response()->json(['status' => 'Token is Expired']);
-            }else{
+            } else {
                 return response()->json(['status' => 'Authorization Token not found']);
             }
         }

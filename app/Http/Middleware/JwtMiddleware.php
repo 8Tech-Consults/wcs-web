f4ff2e87ac181ccfa746ab3f4581fb6ed670135b
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-17 09:39:48 +0300

    getallheaders

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -22,7 +22,7 @@ class JwtMiddleware extends BaseMiddleware
     {
         $headers = getallheaders(); //get header
         //$request->headers->set('Authorization', $headers['authorization']);// set header in request
-        var_dump($headers);
+        var_dump($_SERVER);
         die(); 
         try {
             $headers = apache_request_headers(); //get header

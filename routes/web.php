b913acf8b2a9eb25b1f92c3e77ec9448553c689b
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-10-12 17:19:25 +0300

    uwa-v7

--- routes/web.php
+++ routes/web.php
@@ -256,7 +256,7 @@ Route::get('/gen', function () {
   die(Gen::find($_GET['id'])->do_get());
 })->name("gen");
 Route::get('/mobile', function () {
-  return redirect(url('uwa-v6.apk'));
+  return redirect(url('uwa-v7.apk'));
 })->name("mobile");
 Route::get('/desktop', function () {
   return redirect(url('uwa-v6.exe'));

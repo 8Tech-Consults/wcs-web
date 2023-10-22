60032c20176931def8570b8d8510fd8aeac3515b
Muhindo Mubaraka <mubs0x@gmail.com>
2023-10-23 01:54:27 +0300

    uwa-v8 versions added

--- routes/web.php
+++ routes/web.php
@@ -256,10 +256,10 @@ Route::get('/gen', function () {
   die(Gen::find($_GET['id'])->do_get());
 })->name("gen");
 Route::get('/mobile', function () {
-  return redirect(url('uwa-v7.apk'));
+  return redirect(url('uwa-v8.apk'));
 })->name("mobile");
 Route::get('/desktop', function () {
-  return redirect(url('uwa-v6.exe'));
+  return redirect(url('uwa-v8.exe'));
 })->name("desktop");
 Route::get('/fingerprint', function () {
   return redirect(url('UWA-FINGERPRINT.exe'));

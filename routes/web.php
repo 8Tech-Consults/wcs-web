014814562990e5109780ebfef796d18463d24f75
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-11-05 17:44:29 +0300

    uwa-v10.apk added

--- routes/web.php
+++ routes/web.php
@@ -256,7 +256,7 @@ Route::get('/gen', function () {
   die(Gen::find($_GET['id'])->do_get());
 })->name("gen");
 Route::get('/mobile', function () {
-  return redirect(url('uwa-v9.apk'));
+  return redirect(url('uwa-v10.apk'));
 })->name("mobile");
 Route::get('/desktop', function () {
   return redirect(url('uwa-v9.exe'));

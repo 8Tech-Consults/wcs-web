146f8ede4116c9c04a0f5d495cdbd4b1edacd88f
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-12-25 17:49:37 +0300

    new changes

--- vendor/guzzlehttp/guzzle/src/MessageFormatter.php
+++ vendor/guzzlehttp/guzzle/src/MessageFormatter.php
@@ -45,7 +45,7 @@ class MessageFormatter implements MessageFormatterInterface
      * @var string
      */
     public const CLF = '{hostname} {req_header_User-Agent} - [{date_common_log}] "{method} {target} HTTP/{version}" {code} {res_header_Content-Length}';
-    public const DEBUG = "<\n{response}\n--------\n{error}";
+    public const DEBUG = ">>>>>>>>\n{request}\n<<<<<<<<\n{response}\n--------\n{error}";
     public const SHORT = '[{ts}] "{method} {target} HTTP/{version}" {code}';
 
     /**
@@ -68,7 +68,7 @@ class MessageFormatter implements MessageFormatterInterface
      * @param ResponseInterface|null $response Response that was received
      * @param \Throwable|null        $error    Exception that was received
      */
-    public function format(RequestInterface $request, ?ResponseInterface $response = null, ?\Throwable $error = null): string
+    public function format(RequestInterface $request, ResponseInterface $response = null, \Throwable $error = null): string
     {
         $cache = [];
 

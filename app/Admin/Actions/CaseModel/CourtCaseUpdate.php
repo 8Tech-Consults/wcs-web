4793d3e6a64293b9824e2bf870943fd50b1beee9
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-02-09 17:37:04 +0300

    Update court info in CourtCaseUpdate action

--- app/Admin/Actions/CaseModel/CourtCaseUpdate.php
+++ app/Admin/Actions/CaseModel/CourtCaseUpdate.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourtCaseUpdate extends RowAction
 {
-    public $name = 'Update';
+    public $name = 'Update court info';
 
     public function handle(Model $model)
     {

121a8e3d0a34a1a75292ece66fb3c693d09a95fe
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-03-22 11:48:35 +0300

    as

--- app/Admin/Controllers/CaseSuspectController.php
+++ app/Admin/Controllers/CaseSuspectController.php
@@ -156,7 +156,8 @@ class CaseSuspectController extends AdminController
         $grid->filter(function ($f) {
             // Remove the default id filter
             $f->disableIdFilter();
-            $f->between('created_at', 'Filter by date')->date();
+            $f->between('created_at', 'Filter by date of entry')->date();
+            $f->between('case_date', 'Filter by case date')->date();
             /*             $f->equal('reported_by', "Filter by complainant")
                 ->select(Administrator::all()->pluck('name', 'id')); */
 

9281e93c0a851c3ff65e6c488e7056575f72aba6
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2024-07-30 16:26:25 +0300

    as

--- app/Models/ReportModel.php
+++ app/Models/ReportModel.php
@@ -1046,7 +1046,8 @@ case_date
 
 
         $conds['is_suspects_arrested'] = 'Yes';
-        $conds['is_fined'] = 'Yes';
+        $conds['is_fined'] = 'Yes'; 
+        $conds['is_suspect_appear_in_court'] = 'Yes'; 
 
         return CaseSuspect::where($conds)
             ->whereBetween('case_date', [$this->start_date, $this->end_date])
